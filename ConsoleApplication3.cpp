ebaa78cfbad9b8e0870b757fe5b43afb97e997bb
vvivchar-bit <v.vivchar@istu.edu.ua>
2026-03-22 12:41:31 +0200

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    map<int, string> months;
+
+    months[1] = "January";
+    months[2] = "February";
+    months[3] = "March";
+    months[4] = "April";
+    months[5] = "May";
+    months[6] = "June";
+    months[7] = "July";
+    months[8] = "August";
+    months[9] = "September";
+    months[10] = "October";
+    months[11] = "November";
+    months[12] = "December";
+
+    int number;
+    cout << "Enter month number (1-12): ";
+    cin >> number;
+
+    if (months.find(number) != months.end())
+    {
+        cout << "Month name: " << months[number] << endl;
+    }
+    else
+    {
+        cout << "Invalid month number!" << endl;
+    }
+
+    return 0;
+}
---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main()
+{
+    map<string, string> phoneBook;
+
+    phoneBook["Alice"] = "+380501111111";
+    phoneBook["Bob"] = "+380502222222";
+    phoneBook["Charlie"] = "+380503333333";
+    phoneBook["Diana"] = "+380504444444";
+
+    string name;
+    cout << "Enter name: ";
+    cin >> name;
+
+    if (phoneBook.find(name) != phoneBook.end())
+    {
+        cout << "Phone number: " << phoneBook[name] << endl;
+    }
+    else
+    {
+        cout << "Contact not found!" << endl;
+    }
+
+    return 0;
+}
